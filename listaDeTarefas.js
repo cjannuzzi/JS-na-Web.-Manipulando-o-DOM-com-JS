92ead11c1de13d69f0929c4d6be21abb67767c17
Caio Jannuzzi <eng.caiojannuzzi@outlook.com>
2022-01-19 17:34:32 -0300

    InnerHTML

--- listaDeTarefas.js
+++ listaDeTarefas.js
@@ -4,8 +4,14 @@ const criarTarefa = (evento) => {
 
     const input = document.querySelector('[data-form-input]')
     const valor = input.value
-    console.log( valor )
-    input.value = " "
+
+    
+   const tarefa = document.querySelector('[data-task]')
+
+   const conteudo = `<p class="content">${valor}</p>`
+
+   tarefa.innerHTML = conteudo
+   input.value = " "
 }
 
 const novaTarefa = document.querySelector('[data-form-button]')
