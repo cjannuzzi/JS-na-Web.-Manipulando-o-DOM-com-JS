0b80e8d69a50d6384dff5d6c62ec4b0c72e6a11f
Caio Jannuzzi <eng.caiojannuzzi@outlook.com>
2022-01-20 16:47:17 -0300

    Adicionar botao de concluir tarefa

--- listaDeTarefas.js
+++ listaDeTarefas.js
@@ -9,10 +9,12 @@ const criarTarefa = (evento) => {
 
     const tarefa = document.createElement('li')
     tarefa.classList.add('task')
-    
+
     const conteudo = `<p class="content">${valor}</p>`
 
     tarefa.innerHTML = conteudo
+
+    tarefa.appendChild(BotaoConclui())
     lista.appendChild(tarefa)
     input.value = " "
 }
@@ -20,3 +22,15 @@ const criarTarefa = (evento) => {
 const novaTarefa = document.querySelector('[data-form-button]')
 
 novaTarefa.addEventListener('click', criarTarefa)
+
+const BotaoConclui = () => {
+    const botaoConclui = document.createElement('button')
+
+    botaoConclui.classList.add('check-button')
+    botaoConclui.innerText='concluir'
+    botaoConclui.addEventListener('click', ()=>{
+        console.log( 'fui clicado' )
+    })
+
+    return botaoConclui
+}
