73fc239cd518c585817dd9c3011e2a18efc8ee5e
Caio Jannuzzi <eng.caiojannuzzi@outlook.com>
2022-01-20 22:08:43 -0300

    Atualizar arquivos

--- components/concluiTarefa.js
+++ components/concluiTarefa.js
@@ -1,19 +1,27 @@
-const BotaoConclui = () => {
+//Criando um componente (botão deletar tarefa - componete começa com maiúsculo)
+const BotaoConclui = ()=>{
+    //Criando um elemento
     const botaoConclui = document.createElement('button')
-
+    //Adicionando uma classe ao botão
     botaoConclui.classList.add('check-button')
-    botaoConclui.innerText='concluir'
+    //Adiciona um texto no botão
+    botaoConclui.innerText = 'concluir'
+    //Criando um evento
     botaoConclui.addEventListener('click', concluirTarefa)
 
+    //Retornar o botão
     return botaoConclui
 }
 
+//Criando a função concluir tarefa (Arrow function - função de seta)
 const concluirTarefa = (evento) => {
+    //Retorna em qual elemento eu cliquei(target - alvo clicado)
     const botaoConclui = evento.target
-
+    //Sobe um nó na árvore do DOM (parentElement - pega o pai do elemento clicado, pai do botão é li)
     const tarefaCompleta = botaoConclui.parentElement
-
+    //Insere envento de rabisco na li
     tarefaCompleta.classList.toggle('done')
 }
 
+//exporta o botão concluir com a mesma sintaxe IIFE de protejer o código - Encapsular o código
 export default BotaoConclui
--- components/deletaTarefa.js
+++ components/deletaTarefa.js
@@ -1,20 +1,20 @@
+//Criando um componente (botão deletar tarefa - componete começa com maiúsculo)
 const BotaoDeleta = () => {
     const botaoDeleta = document.createElement('button')
-
     botaoDeleta.innerText = 'deletar'
-    botaoDeleta.addEventListener = ('click', deletarTarefa)
+    botaoDeleta.addEventListener('click', deletarTarefa)
 
     return botaoDeleta
 }
 
 const deletarTarefa = (evento) => {
     const botaoDeleta = evento.target
-
+    //Pega o elemento pai do botãoDeleta que é a li
     const tarefaCompleta = botaoDeleta.parentElement
-
+    //Removendo a li
     tarefaCompleta.remove()
 
     return botaoDeleta
 }
-
+//exporta o botão deleta com a mesma sintaxe IIFE de protejer o código - Encapsular o código
 export default BotaoDeleta
